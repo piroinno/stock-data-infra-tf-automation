23de5c9c6ca4ea3768f9a4409dc871abec0077ac
piroinno <peter.irojah@piroinno.com>
2023-03-17 07:24:36 +0000

    Fixed bug in replace script

--- scripts/Set-PathSlashes.ps1
+++ scripts/Set-PathSlashes.ps1
@@ -7,11 +7,11 @@ function Set-PathSlashes {
     if([environment]::OSVersion.Platform -eq "Win32NT") {
         $Path = $Path.Replace("/", "\")
     }elseif([environment]::OSVersion.Platform -eq "Unix") {
-        $Path = $Path.Replace("/", "\")
+        $Path = $Path.Replace("\", "/")
     }elseif([environment]::OSVersion.Platform -eq "MacOSX") {
-        $Path = $Path.Replace("/", "\")
+        $Path = $Path.Replace("\", "/")
     }elseif([environment]::OSVersion.Platform -eq "Linux") {
-        $Path = $Path.Replace("/", "\")
+        $Path = $Path.Replace("\", "/")
     }
     return $Path 
 }
