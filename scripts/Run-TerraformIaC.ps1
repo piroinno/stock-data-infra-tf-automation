d5bb2431f29ea3fc31ddead7f001f904d21190f9
piroinno <peter.irojah@piroinno.com>
2023-03-17 22:11:27 +0000

    sed

--- scripts/Run-TerraformIaC.ps1
+++ scripts/Run-TerraformIaC.ps1
@@ -174,7 +174,7 @@ process {
         }
         apply {
             Write-Verbose "Starting Terraform Apply"
-            Write-Verbose("terraform apply $TerraformConfigPlanFile -auto-approve -lock-timeout=$TF_LOCK_TIMEOUT")
+            Write-Host("terraform apply $TerraformConfigPlanFile -auto-approve -lock-timeout=$TF_LOCK_TIMEOUT")
             Invoke-Expression("terraform apply $TerraformConfigPlanFile -auto-approve -lock-timeout=$TF_LOCK_TIMEOUT")
             break
         }
